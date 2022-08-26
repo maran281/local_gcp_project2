70b819b74f90c047950b37f9f36bea7f6c1a4a97
Manoj Rana (External - HCL Sweden AB) <manoj.rana@ingka.com>
2022-08-26 07:42:50 +0200

    checkin Aug26_6

--- provider.tf
+++ provider.tf
@@ -4,5 +4,5 @@ provider "google" {
   project     = var.gcp_project
   #credentials = file(var.gcp_auth_file)
   region      = var.gcp_region
-  # credentials = file("C:/gcp_git_poc1/local_gcp_project3/terraform/cla-poc-key.json")
+  credentials = file("C:/gcp_git_poc1/local_gcp_project3/terraform/cla-poc-key.json")
 }
