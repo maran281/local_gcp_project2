98881b08c03325d5f11f968d81bfa8caeb336719
Manoj Rana (External - HCL Sweden AB) <manoj.rana@ingka.com>
2022-08-26 07:27:55 +0200

    checkin backend.tf 26aug_3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "testblablablabucket"
     prefix = "pocState"
     # credentials = "C:/gcp_poc/key/cla-poc-key.json"
-    credentials = file("./cla-poc-key.json")
+    # credentials = file("./cla-poc-key.json")
   }
 } 
